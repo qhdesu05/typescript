c07e79a91f98ca0d640520a6132cb6179fc697d4
qhdesu <hmquangdev@gmail.com>
2025-12-11 12:04:46 +0700

    add DataType

---
+++ src/DataType/String,Number,Boolean/datatype.ts
@@ -0,0 +1,15 @@
+let username: string = "qh"; //string
+username.toLowerCase();
+//error
+//username = 25;
+
+const age1: number = 36; //number
+
+const isBad: boolean = true; //boolean
+
+//coding sai
+let username1: String = "qh"; //string
+
+const age2: Number = 36; //number
+
+const isBad1: Boolean = true; //boolean
