570757236ce54eca9d3dbcaa4e31967d1523a350
qhdesu <hmquangdev@gmail.com>
2025-12-15 00:07:48 +0700

    commit 5

---
+++ src/DataType/TypeAnnotationsTypeInference/server.ts
@@ -0,0 +1,6 @@
+export {};
+//annotation: @Controller @service : cong cu ho tro may moc co the hieu ve phan code <=> //comment
+const name4: string = "qh";
+//infer : ts dua vao gia tri ben tay phai de biet dc kieu du lieu
+let usernamez = "qhdesu";
+let abc = false;
---
+++ src/DataType/anyunknow/server.ts
@@ -0,0 +1,16 @@
+export{};
+
+function test(name:){
+
+}
+//any
+let name: any = "qh";
+
+let name1: string = "qh";
+name.toLowerCase(); //goi y method string
+
+//unknow
+let name2: unknown = "qh";
+if(typeof(name2)=="string"){
+    name2.toLowerCase(); 
+}
