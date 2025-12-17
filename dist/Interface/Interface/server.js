19f3c4658537b6bef7600994c62ef8b4f44b6a17
qhdesu <hmquangdev@gmail.com>
2025-12-17 13:28:03 +0700

    interface

---
+++ dist/Interface/Interface/server.js
@@ -0,0 +1,23 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+;
+const quanghoang = {
+    name: "Quang Hoang",
+    age: 20,
+    language: "vn",
+    sayHi: () => console.log("Hello 1"),
+};
+quanghoang.sayHi();
+const thanhhoa = {
+    name: "Thanh Hoa",
+    age: 36,
+    address: "Raumaia",
+    language: "thanh hoa",
+    sayHi: () => console.log("Hello 2"),
+};
+;
+;
+const myDog = {
+    name: "Buddy",
+    breed: "Husky"
+};
---
+++ src/Interface/Interface/server.ts
@@ -0,0 +1,35 @@
+export{};
+interface IPerson{
+name : string;
+age : number;
+address?: string;
+readonly language :  string; //readonly : ko dc sua doi
+sayHi: () => void;
+};
+
+const quanghoang: IPerson = {
+    name : "Quang Hoang",
+    age : 20,
+    language : "vn",
+    sayHi: () => console.log("Hello 1"),
+};
+quanghoang.sayHi();
+const thanhhoa: IPerson = {
+    name : "Thanh Hoa",
+    age : 36,
+    address :"Raumaia",
+    language : "thanh hoa",
+    sayHi: () => console.log("Hello 2"),
+};
+
+interface Animal{
+name : string;
+};
+interface Dog extends Animal{
+    breed : string;
+};
+const myDog: Dog={
+    name :"Buddy",
+    breed : "Husky"
+};
+
